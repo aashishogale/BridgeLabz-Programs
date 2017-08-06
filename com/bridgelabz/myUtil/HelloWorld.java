393a56e84b9dcb02e6cbea9d971b5b283f8753f4
aashishogale <aashishogale9@gmail.com>
2017-08-06 16:09:36 +0530

    Commit message

---
+++ com/bridgelabz/myUtil/HelloWorld.java
@@ -0,0 +1,18 @@
+/****************
+* Compile javac -d bin HelloWorld.java
+* Execute java -cp com.bridgelabz.myUtil.HelloWorld
+*
+* Purpose:To print Hello world
+*
+*@author aashish
+*@version 1.0
+*@since 6/08/2017
+****************/
+package  com.bridgelabz.myUtil;
+
+public class HelloWorld {
+  public static void main(String args[]) {
+    String message="hello world";
+    System.out.println(message);
+    }
+}
