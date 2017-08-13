c2c7c22973764f28d32759eeaf5f1c7252878537
aashishogale <aashishogale9@gmail.com>
2017-08-13 13:50:41 +0530

    commit message

--- com/bridgelabz/util/Sqrtnewton.java
+++ com/bridgelabz/util/Sqrtnewton.java
@@ -12,19 +12,23 @@ package com.bridgelabz.util;
 
 public class Sqrtnewton {
     public static void main(String args[]) {
-        
-        double c=Double.parseDouble(args[0]);
-   
-        double epsilon =1e-15;
- 
-        double t=c;
-
-        while(Math.abs(t-c/t)>epsilon*t){
-
-        t=(c/t+t)/2.0;
-       
-        }
      
-        System.out.println(t);
+       //declare double variable for number
+        
+       double c=Double.parseDouble(args[0]);
+   
+       double epsilon =1e-15;
+ 
+       double t=c;
+
+
+       //loop until condition is satisfied
+       while(Math.abs(t-c/t)>epsilon*t) {
+
+          t=(c/t+t)/2.0;
+       
+       }
+     
+       System.out.println(t);
    }
 }
